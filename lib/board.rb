784efd94bdf92c4363dc72563a3780498987a230
Emogene Lukoye <lukoyedith@gmail.com>
2019-08-25 14:50:39 -0700

    modified main.rb and board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 class Board
+  attr_accessor :board, :win_combination
   def initialize
     @board = [1,2,3,4,5,6,7,8,9]
+    @win_combination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   end
 
   def display
@@ -12,6 +14,39 @@ class Board
   def instruction
     puts "Welcome to Tic Tac Toe!"
     puts "Choose a number between 1-9: "
+  end
+  
+  def full?
+  @board.all?{|token| token == "x" || token =="o"}
+  end
+
+  def draw?
+    full? && !win?
+  end
+
+  def position_taken? #class game
+    @board[position] == "x" || @board[position] == "o"
+  end
+  
+  def valid? #class game
+    if position.between?(0,8)
+      return true
+    else 
+      puts "Invalid move or input."
+    end
+  end
+
+  def win?
+  default = false
+  @win_combination.each{ |element|
+   brian = [@board[element[0]],@board[element[1]],@board[element[2]]].all? { |i| i == "x" } || [@board[element[0]],@board[element[1]],@board[element[2]]].all? { |i| i == "o" }
+   if brian
+    $winner = @board[element[0]] 
+   end
+    break  if brian
+  }
+  
+    return default
   end
 end 
   
